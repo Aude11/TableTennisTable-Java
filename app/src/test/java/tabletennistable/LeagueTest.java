6bfa093422dea0128d690dbb6d42da4173bbe554
Aude11 <vernet.aude@gmail.com>
2024-04-28 16:41:06 +0100

    add exercice

--- app/src/test/java/tabletennistable/LeagueTest.java
+++ app/src/test/java/tabletennistable/LeagueTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class LeagueTest {
     @Test
     public void testAddPlayer()
@@ -23,4 +25,47 @@ public class LeagueTest {
         Assert.assertEquals(1, firstRowPlayers.size());
         Assert.assertThat(firstRowPlayers, IsCollectionContaining.hasItem("Bob"));
     }
+
+    @Test
+    public void testAddPlayerInOnGoingLeagueNewValidPlayerAddsAtBottom()
+    {
+        // Given
+        League league = new League();
+        league.addPlayer("A");
+
+        // When
+        league.addPlayer("NewPlayer");
+
+        // Then
+        List<LeagueRow> rows = league.getRows();
+        Assert.assertEquals(2, rows.size());
+        List<String> lastRowPlayers = rows.get(1).getPlayers();
+        Assert.assertEquals(1, lastRowPlayers.size());
+        Assert.assertThat(lastRowPlayers, IsCollectionContaining.hasItem("NewPlayer"));
+    }
+
+    @Test
+    public void testAddPlayerInOnGoingLeagueNewInvalidPlayerNotAdded()
+    {
+        // Given
+        League league = new League();
+        league.addPlayer("A");
+        league.addPlayer("B");
+
+        // When
+        //league.addPlayer("A");
+
+        // Then
+        assertThrows(IllegalArgumentException.class,
+                () -> league.addPlayer("A"));
+
+
+//        List<LeagueRow> rows = league.getRows();
+//        Assert.assertEquals(2, rows.size());
+//        List<String> lastRowPlayers = rows.get(1).getPlayers();
+//        Assert.assertEquals(1, lastRowPlayers.size());
+//        Assert.assertThat(lastRowPlayers, IsCollectionContaining.hasItem("B"));
+    }
+
+
 }
