a90f4a3467858ccbdb66c632073ed2f8d8095415
aude <vernet.aude@gmail>
2024-05-30 16:51:24 +0100

    add test

--- app/src/test/java/tabletennistable/AppTest.java
+++ app/src/test/java/tabletennistable/AppTest.java
@@ -6,10 +6,19 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import static org.mockito.Mockito.verify;
 
 public class AppTest {
     @Mock
     LeagueRenderer renderer;
+
+    @Mock
+    FileService fileService;
+
+    @Mock
+    League leagueMock;
 
     @BeforeEach
     public void initMocks() {
@@ -27,4 +36,61 @@ public class AppTest {
 
         Assert.assertEquals("Rendered League", app.sendCommand("print"));
     }
+
+    @Test
+    public void testAddPlayer()
+    {
+        initMocks();
+        App app = new App(leagueMock, renderer, null);
+
+        Assert.assertEquals("Added player Alice", app.sendCommand("add player Alice"));
+        verify(leagueMock).addPlayer("Alice");
+    }
+
+    @Test
+    public void testRecordWinAliceBob()
+    {
+        initMocks();
+        App app = new App(leagueMock, renderer, null);
+
+        Assert.assertEquals("Recorded Alice win against Bob", app.sendCommand("record win Alice Bob"));
+        verify(leagueMock).recordWin("Alice", "Bob");
+    }
+
+    @Test
+    public void testWinner()
+    {
+        initMocks();
+        Mockito.when(leagueMock.getWinner()).thenReturn("Alice");
+
+        App app = new App(leagueMock, renderer, null);
+
+        Assert.assertEquals("Alice", app.sendCommand("winner"));
+    }
+
+    @Test
+    public void testSave()
+    {
+        initMocks();
+        //Mockito.when(leagueMock.getWinner()).thenReturn("Alice");
+
+        App app = new App(leagueMock, renderer, fileService);
+
+        Assert.assertEquals("Saved path", app.sendCommand("save path"));
+        verify(fileService).save("path", leagueMock);
+    }
+
+    @Test
+    public void testLoad()
+    {
+        initMocks();
+        //Mockito.when(leagueMock.getWinner()).thenReturn("Alice");
+
+        App app = new App(leagueMock, renderer, fileService);
+
+        Assert.assertEquals("Loaded path", app.sendCommand("load path"));
+        verify(fileService).load("path");
+    }
+
 }
+
